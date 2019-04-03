b068a9bdc480f3647d38058f50c0dce2a8784913
Maciej Rumas <maciej.rumas@gmail.com>
2019-04-03 08:52:07 +0200

    Error Alert

--- src/CanbanBoard/addItemPopupController.java
+++ src/CanbanBoard/addItemPopupController.java
@@ -2,6 +2,7 @@ package CanbanBoard;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -18,13 +19,19 @@ public class addItemPopupController {
 
     public void pressAddButton(ActionEvent actionEvent){
 
-        Task newTask = new Task(titleTextField.getText(),datePicker.getValue(), Main.getComboBox().getValue(), descriptionBox.getText());
-
-        Controller.getToDoObservableList().add(newTask);
-
-        Controller.getToDoListView().setItems(Controller.getToDoObservableList());
-
-        Controller.getSecondaryStage().close();
+        if(titleTextField.getText().isEmpty() ||  datePicker.getValue() == null || descriptionBox.getText().isEmpty()){
+            Alert error = new Alert(Alert.AlertType.ERROR);
+            error.setContentText("Some values are missing!");
+            error.setHeaderText("Error");
+            error.setTitle("Error");
+            error.show();
+        }
+        else{
+            Task newTask = new Task(titleTextField.getText(),datePicker.getValue(), Main.getComboBox().getValue(), descriptionBox.getText());
+            Controller.getToDoObservableList().add(newTask);
+            Controller.getToDoListView().setItems(Controller.getToDoObservableList());
+            Controller.getSecondaryStage().close();
+        }
     }
 
     public void pressCancelButton(ActionEvent actionEvent){
